90063f62bcd0f30c46404b900193ed843b28a354
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2021-08-14 13:39:38 +0530

    feat: Add gifs before calculations

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const inputBuyingPrice = document.querySelector(".buying-price");
 const inputCurrentPrice = document.querySelector(".current-price");
 const inputQuantity = document.querySelector(".quantity");
 const resultStatement = document.querySelector(".result-statement");
+const loadingGIF = document.querySelector(".loading")
 
 
 function calculate() {
+    calculateButton.style.display = "none";
+    loadingGIF.style.display = "block";
     
     let buyingQuantity = Number(inputQuantity.value);
     let buyingPrice = Number(inputBuyingPrice.value);
@@ -24,7 +27,8 @@ function calculate() {
     } 
     else if(totalValuation >= 1){
         setTimeout(() => {
-            resultStatement.innerText = `Wuhhuuu!! You are in total profit of ${totalValuation} rupees and profit percentage is ${totalPercentage}`;  
+            loadingGIF.style.display = "none";
+            resultStatement.innerText = `Wuhhuuu!! You are in total profit of ${Math.abs(totalValuation)} rupees and profit percentage is ${Math.round(Math.abs(totalPercentage))}%`;  
             upperShade.style.boxShadow = "15px 30px 710px 150px green";
             upperShade.style.transition = "3s ease-out";
                 
@@ -35,7 +39,8 @@ function calculate() {
     }
     else if(totalValuation < 1) {
         setTimeout(() => {
-            resultStatement.innerText = `OOpsie :( You are in loss of ${Math.abs(totalValuation)} rupees and loss percentage is ${Math.abs(totalPercentage)}`;
+            loadingGIF.style.display = "none";
+            resultStatement.innerText = `OOpsie :( You are in loss of ${Math.abs(totalValuation)} rupees and loss percentage is ${Math.round(Math.abs(totalPercentage))}%`;
             upperShade.style.boxShadow = "15px 30px 710px 150px red";
             upperShade.style.transition = "3s ease-out";
                 
